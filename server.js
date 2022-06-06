6052bac5d9bfd49495d877c93fbed69ba63aab27
Breno Lins <brenolins100@gmail.com>
2022-06-06 16:29:06 -0300

    Arquivos inciais para o funcionamento da Api

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+mongoose = require('mongoose')
+
+const produto = require('./src/api/models/produtoModel.js');
+
+const bodyParser = require('body-parser');
+
+mongoose.promise = global.promise;
+mongoose.connect(
+    process.env.DATABASE_URL
+)
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const routes = require('./src/api/routes/produtoRoutes.js');
+routes(app)
+
+
+app.listen(PORT,()=>{
+    console.log('ESCUNTADO A PORTA'+PORT);
+})
---
+++ src/api/models/produtoModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const produtoSchema = new Schema({
+    nome: {
+        type: String
+    },
+    preco: {
+        type: String
+    },
+    criadoEm:{
+        type: Date, default: Date.now
+    },
+    atualizadoEm:{
+        type: Date, default: Date.now
+    },
+    __v:{
+        type: Number, 
+        select: false
+    },
+    emEstoque:{
+        type: Boolean
+    },
+})
+
+module.exports = mongoose.model('produto', produtoSchema);
